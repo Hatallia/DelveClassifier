609899e3626a496414253a9fd95b90193bb32540
Natallia Makarevich <Natallia_Makarevich@epam.com>
2015-11-09 20:47:15 +0300

    Data service changes

--- app/home/home.controller.js
+++ app/home/home.controller.js
@@ -15,27 +15,28 @@
     vm.loading = false;
     vm.documents = [];
     vm.findDocuments = findDocuments;
-
+    vm.getAllBoards = dataService.getAllBoards;
     activate();
 
     function activate() {
-      if (Office.context.document) {
-        Office.context.document.addHandlerAsync(Office.EventType.DocumentSelectionChanged, selectedTextChanged);
-      }
+      // if (Office.context.document) {
+      //   Office.context.document.addHandlerAsync(Office.EventType.DocumentSelectionChanged, selectedTextChanged);
+      // }
+      vm.getAllBoards();
     }
     
-    function selectedTextChanged() {
-      Office.context.document.getSelectedDataAsync(Office.CoercionType.Text,
-        function (result) {
-          if (result.status === Office.AsyncResultStatus.Succeeded) {
-            vm.searchQuery = result.value;
-            $scope.$apply();
-          }
-          else {
-            console.error(result.error.message);
-          }
-        });
-    }
+    // function selectedTextChanged() {
+    //   Office.context.document.getSelectedDataAsync(Office.CoercionType.Text,
+    //     function (result) {
+    //       if (result.status === Office.AsyncResultStatus.Succeeded) {
+    //         vm.searchQuery = result.value;
+    //         $scope.$apply();
+    //       }
+    //       else {
+    //         console.error(result.error.message);
+    //       }
+    //     });
+    // }
 
     function searchQueryKeyDown($event) {
       if ($event.keyCode === 13 &&
--- app/services/data.service.js
+++ app/services/data.service.js
@@ -11,7 +11,8 @@
     
     // public signature of the service
     return {
-      getDocuments: getDocuments
+      getDocuments: getDocuments,
+      getAllBoards: getAllBoards
     };
 
     /** *********************************************************** */
@@ -38,33 +39,10 @@
     function getDocuments(query) {
       var deferred = $q.defer();
  var searchQuery = "?QueryText=%27Path:TAG://PUBLIC/?NAME=" + query + "%27&Properties=%27IncludeExternalContent:true%27&SelectProperties=%27DocId,Title,Path%27&RankingModelId=%270c77ded8-c3ef-466d-929d-905670ea1d72%27";
-    //  var searchQuery = "?querytext='" + query + " isdocument:1'&SelectProperties='HitHighlightedSummary,LastModifiedTime,Path,SPWebUrl,ServerRedirectedURL,SiteTitle,Title'&RowLimit=5&StartRow=0";
- //var searchQuery = "?QueryText=%27Path:TAG://PUBLIC/?NAME=*%27&Properties=%27IncludeExternalContent:true%27&SelectProperties=%27DocId,Title,Path%27&RankingModelId=%270c77ded8-c3ef-466d-929d-905670ea1d72%27";
+ 
+    searchQuery = "?QueryText=%27Path:TAG://PUBLIC/?NAME=*%27&Properties=%27IncludeExternalContent:true%27&SelectProperties=%27DocId,Title,Path%27&RankingModelId=%270c77ded8-c3ef-466d-929d-905670ea1d72%27";
 
-      
-
-         
-// var signal = [{"Type":"Tag","TagName":"TEST",
-//   "DocumentUrl":"https://fancycoder-my.sharepoint.com/personal/natalliamakarevich_fancycoder_onmicrosoft_com/Documents/Document1.docx"},
-//  {"Type":"Follow","TagName":"TAG://PUBLIC/?NAME=TEST"}];
-
-
-//       $http({
-//         url: "https://fancycoder-my.sharepoint.com/_vti_bin/DelveApi.ashx/signals/batch",
-//         method: 'POST',
-//         headers: {
-//           'Accept': 'application/json;odata=nometadata',
-//           'Content-Type':'application/json;odata=verbose',
-//           'Host':'fancycoder-my.sharepoint.com',
-//           'Origin':'https://fancycoder-my.sharepoint.com',
-
-//           "Access-Control-Allow-Origin":"*",
-//           'X-RequestDigest':query,
-//           'X-Delve-ClientPlatform':'DelveWeb'
-
-//         },
-//         data:JSON.stringify(signal)
-      $http({
+    $http({
         url: sharePointUrl + '/_api/search/query' + searchQuery,
         method: 'GET',
         headers: {
@@ -103,5 +81,52 @@
       return deferred.promise;
     }
     
+  
+
+
+
+
+
+ function getAllBoards() {
+      var deferred = $q.defer();
+
+   var searchQuery = "?QueryText=%27Path:TAG://PUBLIC/?NAME=*%27&Properties=%27IncludeExternalContent:true%27&SelectProperties=%27DocId,Title,Path%27&RankingModelId=%270c77ded8-c3ef-466d-929d-905670ea1d72%27";
+
+    $http({
+        url: sharePointUrl + '/_api/search/query' + searchQuery,
+        method: 'GET',
+        headers: {
+          'Accept': 'application/json;odata=nometadata'
+        }
+      }).success(function (data) {
+
+        var documents = [];
+
+        if (data.PrimaryQueryResult !== null) {
+          data.PrimaryQueryResult.RelevantResults.Table.Rows.forEach(function (row) {
+            var cells = row.Cells;
+
+            var url = getValueFromResults('ServerRedirectedURL', cells);
+            if (url === null) {
+              url = getValueFromResults('Path', cells);
+            }
+
+            documents.push({
+              url: url,
+              title: getValueFromResults('Title', cells)              
+            });
+          });
+        }
+
+        deferred.resolve(documents);
+      }).error(function (err) {
+        deferred.reject(err);
+      });
+
+      return deferred.promise;
+    }
+    
   }
+
+
 })();
