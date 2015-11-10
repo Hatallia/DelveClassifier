c97fe35d873b04214b1ffc45413545ac6efd2561
Natallia Makarevich <Natallia_Makarevich@epam.com>
2015-11-11 02:49:48 +0300

    Put some lipstick on this code

--- scripts/addBoard.js
+++ scripts/addBoard.js
@@ -1,15 +1,12 @@
 <script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
 <script type="text/javascript">
-function AddToDelve() {
+function AddToDelve(board, docLocation) {
 
-    var appWebUrl = window.location.protocol + "//" + window.location.host
-                + _spPageContextInfo.webServerRelativeUrl;
-    var board = $("#board").val();
+    var appWebUrl = window.location.protocol + "//" + window.location.host + _spPageContextInfo.webServerRelativeUrl;
+    
     var data1 = {
-        "signals": 
-[{"Type":"Tag","TagName":"TEST",
-  "DocumentUrl":"https://fancycoder-my.sharepoint.com/personal/natalliamakarevich_fancycoder_onmicrosoft_com/Documents/Document1.docx"},
- {"Type":"Follow","TagName":"TAG://PUBLIC/?NAME=TEST"}]
+        "signals": [{"Type":"Tag","TagName":board, "DocumentUrl" : docLocation},
+        {"Type":"Follow","TagName":"TAG://PUBLIC/?NAME=" + board}]
     };
 
 
@@ -19,14 +16,11 @@ function AddToDelve() {
         headers: { "Accept": "application/json; odata=verbose" }
     }).done(function (data) {
         var validRequestDigest = data.d.GetContextWebInformation.FormDigestValue;
-       // debugger;
-        
-
+       
         var requestHeaders = {
-            "Accept": "application/json;odata=verbose",
-            "Access-Control-Allow-Origin": "https://fancycoder.sharepoint.com",
+            "Accept": "application/json;odata=verbose",            
             "X-RequestDigest": validRequestDigest,
-	    "X-Delve-ClientPlatform":"DelveWeb"
+	        "X-Delve-ClientPlatform":"DelveWeb"
         };
 
         jQuery.ajax({
