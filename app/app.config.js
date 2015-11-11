c15fc80738cd4e4dd3a8275d79d95c1df6877d15
Natallia Makarevich <Natallia_Makarevich@epam.com>
2015-11-11 04:01:16 +0300

    Changing proxy page

--- app/app.config.js
+++ app/app.config.js
@@ -4,4 +4,5 @@
 	var officeAddin = angular.module('officeAddin');
 	officeAddin.constant('appId', '54306a7c-fce1-4899-8f9d-8b21c9937c69');
 	officeAddin.constant('sharePointUrl', 'https://fancycoder.sharepoint.com');
+	officeAddin.constant('proxyHackUrl', 'https://fancycoder.sharepoint.com/sites/Dev/SitePages/DelveClassifierProxy.aspx');
 })();
